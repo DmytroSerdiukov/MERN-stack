1cdcd3c74471b2d7534755ce25ddf42ec5bd3d59
Dmytro Serdiukov <dmytro.serdiukov@gmail.com>
2020-10-16 22:30:42 +0300

    added Typescript in Orders

--- client/src/pages/Orders/Orders.tsx
+++ client/src/pages/Orders/Orders.tsx
@@ -1,7 +1,23 @@
-import React, {useHistory} from 'react'
+import React from 'react'
 
-const Order = ({id, name, price, quantity, variant}, ...props) => {
-  const showId = (id) => console.log(id)
+interface Spreaded {
+  id: string,
+  name: string,
+  price: string,
+  quantity: number,
+  variant: string,
+}
+
+type Order = {
+  _id: any,
+  name: string,
+  price: string,
+  quantity: number,
+  variant: string
+}
+
+const Order = ({id, name, price, quantity, variant}: Spreaded) => {
+  const showId = (id: string) => console.log(id)
   return (
   <div style={{border: "1px solid black"}} onClick={ () => showId(id)}>
     <div>
@@ -19,11 +35,11 @@ const Order = ({id, name, price, quantity, variant}, ...props) => {
   </div>
 )}
 
-const Orders = ({orders}) => {
+const Orders = ({orders}: any) => {
   return <div>
     <div>
       {orders === null ? "Заказов нет":
-      orders.map((el, i) => (
+      orders.map((el: Order, i: number) => (
         <Order  
           key = {i}
           id = {el._id || null}
